92db8c6ae4ff35938a27dcdb128ec26819760491
DeedleFake <deedlefake@users.noreply.github.com>
2022-04-03 14:09:32 -0400

    Initial Commit.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/DeedleFake/fyner
+
+go 1.18
+
+require fyne.io/fyne/v2 v2.1.4 // indirect
---
+++ state/state.go
@@ -0,0 +1,63 @@
+package fyner
+
+// State represents a value that can change over time.
+type State[T any] interface {
+	// Listen registers a listener function to be called whenever the
+	// State's value changes.
+	//
+	// When Listen is first called, it immedietely calls the provided
+	// function with the current value of the state and does not return
+	// until after the provided function returns.
+	Listen(func(T)) CancelFunc
+}
+
+// Getter is an optional interface defined by States to allow for
+// quicker one-time fetching of their current value.
+type Getter[T any] interface {
+	Get() T
+}
+
+// Setter represents a value that can be changed.
+type Setter[T any] interface {
+	Set(v T)
+}
+
+// MutableState is a State that can be changed. There is no guarantee
+// about the relationship between the Set method returning and the
+// State's listeners being informed of the update.
+type MutableState[T any] interface {
+	State[T]
+	Setter[T]
+}
+
+// Get returns the current value of a given state.
+func Get[T any](s State[T]) (v T) {
+	if g, ok := s.(Getter[T]); ok {
+		return g.Get()
+	}
+
+	var cancel CancelFunc
+	cancel = s.Listen(func(c T) {
+		defer cancel()
+		v = c
+	})
+	return v
+}
+
+type CancelFunc func()
+
+type static[T any] struct {
+	v T
+}
+
+// Static returns a state that never changes. If a value is definitely
+// constant, using this type of state is more efficient than a mutable
+// one.
+func Static[T any](v T) State[T] {
+	return static[T]{v}
+}
+
+func (s static[T]) Listen(listener func(T)) CancelFunc {
+	listener(s.v)
+	return func() {}
+}
