b1b791d1a99778a4fb6acba87c1f18482d6ac498
DeedleFake <deedlefake@users.noreply.github.com>
2022-04-04 17:35:24 -0400

    state: expand inference capability of `ToSliceOfStates()`

--- state/util.go
+++ state/util.go
@@ -3,8 +3,8 @@ package state
 // ToSliceOfStates returns a state derived from s that wraps each
 // element in a Static state. This is useful for, for example, simple
 // usages of fyner.List.
-func ToSliceOfStates[T any, S ~[]T](s State[S]) State[[]State[T]] {
-	return Derived(s, func(v S) []State[T] {
+func ToSliceOfStates[T any, L ~[]T, S State[L]](s S) State[[]State[T]] {
+	return Derived(s, func(v L) []State[T] {
 		r := make([]State[T], 0, len(v))
 		for _, v := range v {
 			r = append(r, Static(v))
