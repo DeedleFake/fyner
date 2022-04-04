478009bf94975575e42db03969e70aa22b56e740
DeedleFake <deedlefake@users.noreply.github.com>
2022-04-04 19:36:32 -0400

    fyner: add `Disabled` to `Entry`

--- entry.go
+++ entry.go
@@ -19,6 +19,10 @@ type Entry struct {
 	// Text is the editable text currently in the entry.
 	Text       state.MutableState[string]
 	textCancel state.CancelFunc
+
+	// Disabled, if true, prevents user input to the text field.
+	Disabled       state.State[bool]
+	disabledCancel state.CancelFunc
 }
 
 func (entry *Entry) init() {
@@ -36,11 +40,25 @@ func (entry *Entry) Bind() {
 	entry.init()
 	entry.Unbind()
 
-	entry.textCancel = entry.Text.Listen(entry.w.SetText)
-	entry.w.OnChanged = entry.Text.Set
+	if entry.Text != nil {
+		entry.textCancel = entry.Text.Listen(entry.w.SetText)
+		entry.w.OnChanged = entry.Text.Set
+	}
+
+	if entry.Disabled != nil {
+		entry.disabledCancel = entry.Disabled.Listen(func(disabled bool) {
+			if disabled {
+				entry.w.Disable()
+				return
+			}
+			entry.w.Enable()
+		})
+	}
 }
 
 func (entry *Entry) Unbind() {
 	cancel(&entry.textCancel)
 	entry.w.OnChanged = nil
+
+	cancel(&entry.disabledCancel)
 }
